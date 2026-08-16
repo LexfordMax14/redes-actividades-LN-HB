e872f4f54ad1ac9d8affc749e0e304d46bd6cbe9
Hector Bonilla <hector.bonilla@ug.uchile.cl>
2026-08-16 17:28:47 -0400

    Ahora el server puede responder a un curl -i.
    Refactors por ahi y Formateos de estilo por alla.

--- parser.py
+++ parser.py
@@ -1,5 +1,29 @@
 import socket
 
+BODY = (
+	"<!DOCTYPE html>\n"
+	'<html lang="es">\n'
+	"<head>\n"
+	'    <meta charset="UTF-8">\n'
+	"    <title>Server HL</title>\n"
+	"</head>\n"
+	"<body>\n"
+	"    <h1>Has usado los servicios HL</h1>\n"
+	"    <h3>Si puedes ver este mensaje, es que has logrado una respuesta de\n"
+	"    nuestro server. Abre una botella de champagne.</h3>\n"
+	"</body>\n"
+	"</html>\n"
+)
+
+HTTP_RESPONSE = (
+	"HTTP/1.1 200 OK\r\n"
+	"Content-Type: text/html; charset=utf-8\r\n"
+	f"Content-Length: {len(BODY.encode('utf-8'))}\r\n"
+	"X-ElQuePregunta: HL\r\n"
+	"Connection: close\r\n"
+	"\r\n" + BODY
+).encode("utf-8")
+
 
 class Http_HL:
 	"""Clase Http Hector-Lazaro.
@@ -8,17 +32,29 @@ class Http_HL:
 
 	def __init__(
 		self,
-		head: dict[str, str] = {},
+		head: dict[str, str] | None = None,
 		start_line: str = "",
 		body: str = "",
 	):
 		self.start_line = start_line
-		self.head = head
+		empty_head: dict[str, str] = {}
+		self.head = empty_head if not head else head
 		self.body = body
 
 
 def parse_HTTP_message(http_message: bytes) -> Http_HL:
-	head, body = http_message.split(b"\r\n\r\n", 1)
+	message = http_message.split(b"\r\n\r\n", 1)
+
+	# TODO(Hector): puede ser que un mensaje HTTP llegue con BODY o no.
+	# Si llega con BODY, hay que rescatar el HEADER `content-length` para saber
+	# de cuantos bytes es el body, y pegarlo al Http_HL
+	# `curl` no envia un content-length, pero un navegador puede.
+	if len(message) < 2:
+		head = message[0]
+		body = b""
+	else:
+		head = message[0]
+		body = message[1]
 
 	http_hl = Http_HL()
 	http_hl.body = body.decode()
@@ -49,14 +85,11 @@ def create_HTTP_message(parse_http: Http_HL) -> bytes:
 def recive_message(socket: socket.socket, buff_size: int) -> bytes:
 	message = b""
 
-	while True:
+	# Convención de http
+	while b"\r\n\r\n" not in message:
 		data = socket.recv(buff_size)
 
-		if data == b"":
-			break
-		# Nuestra convención para el final de un mensaje.
-		if b"\0" in data:
-			message += data
+		if not data:
 			break
 
 		message += data
--- tcp_socket_server.py
+++ tcp_socket_server.py
@@ -1,33 +1,37 @@
 import socket
 
-from parser import recive_message
+from parser import (
+	HTTP_RESPONSE,
+	create_HTTP_message,
+	parse_HTTP_message,
+	recive_message,
+)
 
 if __name__ == "__main__":
+	# IP_VM = '10.0.2.15' # IP de la máquina virtual
+	IP_VM = "127.0.0.1" # si falla la MV
+	buff_size = 4
+	address = (IP_VM, 5000)
 
-	IP_VM = '10.0.2.15' # IP de la máquina virtual
-	# IP_VM = '127.0.0.1' # si falla la MV
+	print("Creando socket - Servidor")
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.bind(address)
+	server_socket.listen(3)
 
-	buff_size = 4 # tamaño del buffer para recibir el mensaje
-	address = (IP_VM, 5000) # dirección del servidor al que nos queremos conectar
-
-	print('Creando socket - Servidor')
-
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creación del socket TCP del servidor
-	server_socket.bind(address)	#asociación del socket a la dirección y puerto especificados
-	server_socket.listen(3) #máximo de conexiones en cola que puede tener el servidor
-
-	# nos quedamos esperando a que llegue una petición de conexión
-	print('... Esperando clientes')
+	print("... Esperando clientes")
 
 	while True:
 		new_socket, new_socket_address = server_socket.accept()
 
 		recv_message = recive_message(new_socket, buff_size)
-		print(f' -> Se ha recibido el siguiente mensaje: {recv_message.decode()}')
+		http_hl = parse_HTTP_message(recv_message)
+		print(
+			" -> Se ha recibido el siguiente mensaje:\n"
+			f"{create_HTTP_message(http_hl).decode()}\n"
+			"---"
+		)
 
-		response_message = f"echo: {recv_message.decode()}"
-		new_socket.send(response_message.encode())
-		new_socket.send(b"\0")
+		new_socket.send(HTTP_RESPONSE)
 
 		new_socket.close()
 		print(f"conexión con {new_socket_address} ha sido cerrada")
